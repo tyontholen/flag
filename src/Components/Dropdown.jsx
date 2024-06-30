af7948b0b008f1c938611578f12b0d3ff293b92c
Gustav Tholén <tyontholen@gmail.com>
2024-06-30 18:57:33 +0200

    bugfix search, color svg filter

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -5,57 +5,59 @@ import './Dropdown.css'; // import CSS for dropdown
 const Dropdown = ({ onChange, theme }) => {
   return (
     <div className={`dropdown_container ${theme === 'light' ? 'light-theme' : ''}`}>
-      <Box sx={{ minWidth: 180 }}>
-        <FormControl fullWidth>
-          <InputLabel
-            sx={{
-              color: 'var(--text-color)',
-              '&.Mui-focused': {
-                color: 'var(--text-color)',
-              },
-            }}
-          >
-            Filter by region
-          </InputLabel>
-          <Select
-            onChange={(e) => onChange(e.target.value)}
-            label="Filter by Region"
-            MenuProps={{
-             
-              disableScrollLock: true, 
-            }}
-            sx={{
-              color: 'var(--text-color)',
-              '& .MuiSelect-select': {
-                backgroundColor: 'var(--primary)',
-              },
-              '& .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'var(--text-color)',
-              },
-              '&:hover .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'var(--text-color)',
-              },
-              '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'var(--text-color)',
-              },
-              '& .MuiMenu-paper': {
-                backgroundColor: 'var(--primary)',
-              },
-              '& .Mui-selected': {
-                backgroundColor: 'var(--secondary) !important',
-                color: 'var(--text) !important',
-              },
-            }}
-          >
-            <MenuItem value="">All continents</MenuItem>
-            <MenuItem value="Africa">Africa</MenuItem>
-            <MenuItem value="Americas">America</MenuItem>
-            <MenuItem value="Asia">Asia</MenuItem>
-            <MenuItem value="Europe">Europe</MenuItem>
-            <MenuItem value="Oceania">Oceania</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
+    <Box sx={{ minWidth: 180 }}>
+  <FormControl fullWidth>
+    <InputLabel
+      sx={{
+        color: 'var(--text-color)',
+        '&.Mui-focused': {
+          color: 'var(--text-color)',
+        },
+      }}
+    >
+      Filter by region
+    </InputLabel>
+    <Select
+      onChange={(e) => onChange(e.target.value)}
+      label="Filter by Region"
+      MenuProps={{
+        disableScrollLock: true, 
+      }}
+      sx={{
+        color: 'var(--text-color)',
+        '& .MuiSelect-select': {
+          backgroundColor: 'var(--primary)',
+        },
+        '& .MuiOutlinedInput-notchedOutline': {
+          borderColor: 'var(--text-color)',
+        },
+        '&:hover .MuiOutlinedInput-notchedOutline': {
+          borderColor: 'var(--text-color)',
+        },
+        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+          borderColor: 'var(--text-color)',
+        },
+        '& .MuiMenu-paper': {
+          backgroundColor: 'var(--primary)',
+        },
+        '& .Mui-selected': {
+          backgroundColor: 'var(--secondary) !important',
+          color: 'var(--text) !important',
+        },
+        '& svg': {
+          color: 'var(--text-color)', 
+        },
+      }}
+    >
+      <MenuItem value="">All continents</MenuItem>
+      <MenuItem value="Africa">Africa</MenuItem>
+      <MenuItem value="Americas">America</MenuItem>
+      <MenuItem value="Asia">Asia</MenuItem>
+      <MenuItem value="Europe">Europe</MenuItem>
+      <MenuItem value="Oceania">Oceania</MenuItem>
+    </Select>
+  </FormControl>
+</Box>
     </div>
   );
 }
